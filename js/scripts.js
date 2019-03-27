86c00dbc8d0968669df93b41dd0270cae26e0d52
Evan Hadfield and Reese Lee <evan@>
2019-03-27 11:05:50 -0700

    edit variable artofcloth to camelCase artOfCloth and that will fix the form and basically the entire page (allows it to collect data and then publish the story.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var adj1Input = $("input#adj1").val();
     var adj2Input = $("input#adj2").val();
-    var adj3Input= $("input#adj3").val();
-    var adj4Input= $("input#adj4").val();
-    var artoclothInput= $("input#artofcloth").val();
+    var adj3Input = $("input#adj3").val();
+    var adj4Input = $("input#adj4").val();
+    var artOfClothInput = $("input#artOfCloth").val();
     var exclamationInput = $("input#exclamation").val();
     var geoInput = $("input#geo").val();
     var nounInput = $("input#noun").val();
@@ -13,13 +13,13 @@ $(document).ready(function() {
     var plurN2Input = $("input#plurN2").val();
     var Spanish1Input = $("input#Spanish1").val();
     var Spanish2Input = $("input#Spanish2").val();
-    var plurN2Input = $("input#plurN2").val();
+    var plurN3Input = $("input#plurN3").val();
 
     $(".adj1").text(adj1Input);
     $(".adj2").text(adj2Input);
     $(".adj3").text(adj3Input);
     $(".adj4").text(adj4Input);
-    $(".artofcloth").text(artofclothInput);
+    $(".artOfCloth").text(artOfClothInput);
     $(".exclamation").text(exclamationInput);
     $(".geo").text(geoInput);
     $(".noun").text(nounInput);
@@ -28,7 +28,7 @@ $(document).ready(function() {
     $(".plurN2").text(plurN2Input);
     $(".Spanish1").text(Spanish1Input);
     $(".Spanish2").text(Spanish2Input);
-    $(".plurN2").text(plurN2Input);
+    $(".plurN3").text(plurN3Input);
 
     $("#story").show();
 
