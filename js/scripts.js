65cc537925b5df2c7646e342009fc2b03857b7ae
Nathan Aden and Reese Lee <nathan@>
2019-04-02 09:46:00 -0700

    modify js file to looping method

--- js/scripts.js
+++ js/scripts.js
@@ -2,56 +2,15 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
 
 
-    var values = ["adj", "adj2", "adj3", "adj4"];
+    var values = ["adj1", "adj2", "adj3", "adj4", "artOfCloth", "exclamation", "geo",
+                  "noun", "bodypart", "plurN1", "plurN2", "plurN3", "Spanish1", "Spanish2", "plurN3"];
 
-    values.forEach(function(value) {
-      var userInput = $("input#" + value).val();
-      $("." + value).text(userInput);
-    });
+      for(var term = 0; term < values.length; term +=1) {
+        var lib = $("#" + values[term]).val();
+        $("." + values[term]).text(lib);
+      }
 
       $("#story").show();
-
       event.preventDefault();
     });
   });
-
-
-
-
-
-  //
-  //   var adj1Input = $("input#adj1").val();
-  //   var adj2Input = $("input#adj2").val();
-  //   var adj3Input = $("input#adj3").val();
-  //   var adj4Input = $("input#adj4").val();
-  //   var artOfClothInput = $("input#artOfCloth").val();
-  //   var exclamationInput = $("input#exclamation").val();
-  //   var geoInput = $("input#geo").val();
-  //   var nounInput = $("input#noun").val();
-  //   var bodypartInput = $("input#bodypart").val();
-  //   var plurN1Input = $("input#plurN2").val();
-  //   var plurN2Input = $("input#plurN2").val();
-  //   var Spanish1Input = $("input#Spanish1").val();
-  //   var Spanish2Input = $("input#Spanish2").val();
-  //   var plurN3Input = $("input#plurN3").val();
-  //
-  //   $(".adj1").text(adj1Input);
-  //   $(".adj2").text(adj2Input);
-  //   $(".adj3").text(adj3Input);
-  //   $(".adj4").text(adj4Input);
-  //   $(".artOfCloth").text(artOfClothInput);
-  //   $(".exclamation").text(exclamationInput);
-  //   $(".geo").text(geoInput);
-  //   $(".noun").text(nounInput);
-  //   $(".bodypart").text(bodypartInput);
-  //   $(".plurN1").text(plurN1Input);
-  //   $(".plurN2").text(plurN2Input);
-  //   $(".Spanish1").text(Spanish1Input);
-  //   $(".Spanish2").text(Spanish2Input);
-  //   $(".plurN3").text(plurN3Input);
-  //
-  //   $("#story").show();
-  //
-  //   event.preventDefault();
-  // });
-// });
